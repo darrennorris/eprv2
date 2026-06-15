833075399eda5838e7e429ad9e702064e7ff3aa7
Darren Norris <doon75@hotmail.com>
2026-06-15 15:34:07 -0300

    add mapview

--- publish_notes.R
+++ publish_notes.R
@@ -3,10 +3,8 @@
 # to update .pdf and .html
 system("quarto publish gh-pages")
 
-# If you are putting this inside an R script and want to ensure the 
-# command runs completely automatically without pausing to ask you for 
-# a "Yes/No" confirmation in the console, you can use system2() and
-# add the --no-prompt flag:
+# So command runs automatically without asking for 
+# a user confirmation in the console:
 system2("quarto", args = c("publish", "gh-pages", "--no-prompt", "--no-browser"))
 
 # OR
